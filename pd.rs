6a9c7c2180b9b0fed19d78cb1431293429b2f298
deseven <de7@deseven.info>
2017-10-05 12:32:35 +0200

    checking get query

--- pd.rs
+++ pd.rs
@@ -4,34 +4,40 @@ use std::thread;
 use std::env;
 use std::process::Command;
 
-fn handle_read(mut stream: &TcpStream) {
+fn handle_read(mut stream: &TcpStream) -> String {
     let mut buf = [0u8 ;4096];
+    let mut request = String::from("");
     match stream.read(&mut buf) {
         Ok(_) => {
-            //return String::from_utf8_lossy(&buf);
             println!("Request from {}!",stream.peer_addr().unwrap());
-            },
+            request = String::from_utf8_lossy(&buf).to_lowercase();
+        },
         Err(e) => println!("Stream read error: {}",e),
     }
+    return request;
 }
 
-fn handle_write(mut stream: TcpStream) {
-    let args: Vec<String> = env::args().collect();
-    let action = &args[2];
-    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{res:\"ok\"}\r\n";
+fn handle_write(mut stream: TcpStream,response: &[u8]) {
+    //let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{res:\"ok\"}\r\n";
     match stream.write(response) {
         Ok(_) => {} //println!("Response to {} sent!",stream.peer_addr().unwrap()),
         Err(e) => println!("Failed sending response: {}",e),
     }
-    match Command::new("/bin/sh").args(&["-c",action]).spawn() {
-        Ok(_) => {} //println!("Action executed!"),
-        Err(e) => println!("Action error: {}",e)
-    }
 }
 
 fn handle_client(stream: TcpStream) {
-    handle_read(&stream);
-    handle_write(stream);
+    let args: Vec<String> = env::args().collect();
+    let action = &args[2];
+    let request = handle_read(&stream);
+    if request.starts_with("get /pull") == true {
+        handle_write(stream,b"HTTP/1.1 200 OK\r\nContent-Type: application/json; charset=UTF-8\r\n\r\n{res:\"ok\"}\r\n");
+        match Command::new("/bin/sh").args(&["-c",action]).spawn() {
+            Ok(_) => println!("Action executed!"),
+            Err(e) => println!("Action error: {}",e)
+        }
+    } else {
+        handle_write(stream,b"HTTP/1.1 403 Forbidden\r\nContent-Type: application/json; charset=UTF-8\r\n\r\n{res:\"failed\"}\r\n");
+    }
 }
 
 fn main() {
